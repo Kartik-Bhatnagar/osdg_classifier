e8152ae081158c058f7b78b2aa85e82fe5e99d98
Kartik-Bhatnagar <80669502+Kartik-Bhatnagar@users.noreply.github.com>
2021-11-21 14:14:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,96 @@
+
+  
+from os import write
+import streamlit as st
+import nltk
+
+from nltk.corpus import stopwords
+from nltk.cluster.util import cosine_distance
+import numpy as np
+import networkx as nx
+import pickle
+import numpy as np
+import pandas as pd
+
+#from transformers import pipeline
+
+header = st.beta_container()
+body = st.beta_container()
+summary_container = st.beta_container()
+
+######################## Summarization code  ########################################
+
+
+def classify(a):
+    filename = 'model2.pickle'
+    model_reloaded = pickle.load(open(filename, 'rb'))
+    
+    te =[]
+    te.append(a)
+    ab = model_reloaded.predict_proba(te)
+    np.set_printoptions(formatter={'float_kind':'{:f}'.format})
+    result = ab.tolist()
+    test_res = result[0]
+    li_goals = ["No Poverty","Zero Hunger","Good Healthand Well Being","Quality Education"
+            ,"Gender Equality","Clean Water and Sanitation","Affordable  and Clean Energy"
+            ,"Decent Work and Economic Growth","Industry,Innovation and Infrastructure"
+            ,"Reduced Inequalites","Sustainable Cities and Communities",
+            "Responsible Consumption and Production","Climate Action","Life Below Water","Life On Land"]
+    t =zip(li_goals,test_res)
+    df_predic = pd.DataFrame(t,columns=["OSDG","Score"])
+    df_predic.index = df_predic.index + 1
+    fi= df_predic.sort_values("Score", ascending = [False])
+    return((fi))
+    
+
+
+# This was a trial for abstractive summarization using transformers which works well but too slow
+# def abstractive(rawtext):
+#    summarizer = pipeline("summarization")
+#    summary = summarizer(rawtext, max_length=300,
+#                         min_length=200, do_sample=False)
+#    summ = summary[0]
+#    return summ['summary_text']
+
+######################## Frontend code  ##############################################
+
+
+with header:
+    st.title('OSDG Classifier')
+
+with body:
+    st.header('Osdg classification')
+    rawtext = st.text_area('Enter Text Here')
+
+    sample_col, upload_col = st.beta_columns(2)
+    sample_col.header('Or select a sample file from below')
+    sample = sample_col.selectbox('Or select a sample file',
+                                  ('kalam_speech.txt', 'Stocks_ FRI_ JUN _8.txt', 'microsoft.txt', 'None'), index=3)
+    if sample != 'None':
+        file = open(sample, "r", encoding='utf-8')
+        #st.write(file)
+        rawtext = file.read()
+
+    upload_col.header('Or upload text file here')
+    uploaded_file = upload_col.file_uploader(
+        'Choose your .txt file', type="txt")
+    if uploaded_file is not None:
+        rawtext = str(uploaded_file.read(), 'utf-8')
+
+    no_of_lines = st.slider("Select number of lines in summary", 1, 5, 3)
+    if st.button('Get Summary'):
+        with summary_container:
+            if rawtext == "":
+                st.header('Summary :)')
+                st.write('Please enter text to see summary')
+            else:
+                result = classify(rawtext)
+                st.write(result)
+
+                # Abstractive summary
+                #st.header('Abstractive method')
+                #abstract = abstractive(rawtext)
+                # st.write(abstract)
+
+                st.header('Actual article')
+                st.write(rawtext)
